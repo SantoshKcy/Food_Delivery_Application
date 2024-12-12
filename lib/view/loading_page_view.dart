f5f84e3de1cc36a332e58b141dcb70ecd6239f3e
SantoshKcy <kcsantosh2003@gmail.com>
2024-12-12 22:23:23 +0545

    loading page added

---
+++ lib/view/loading_page_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class LoadingPageView extends StatelessWidget {
+  const LoadingPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // App Logo
+            Image.asset(
+              'assets/images/logo.png',
+              height: 100,
+            ),
+            const SizedBox(height: 5),
+            // App Name and Tagline
+            const Column(
+              children: [
+                SizedBox(height: 1),
+                Text(
+                  'Your favorite food, just a tap away!',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 40),
+            // Progress Indicator
+            // const CircularProgressIndicator(
+            //   valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
