370b6e158e68c82482cd2b1f0c7aadceed751f7a
SantoshKcy <kcsantosh2003@gmail.com>
2025-01-16 17:15:09 +0545

    changes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:food_delivery_application/app/app.dart';
+import 'package:food_delivery_application/app/di/di.dart';
+import 'package:food_delivery_application/core/network/hive_service.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveService().init();
+
+  await initDependencies();
+  runApp(
+    App(),
+  );
+}
