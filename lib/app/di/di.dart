98f1e4c164c013af335bb2df7064cdeb3296b0fe
SantoshKcy <kcsantosh2003@gmail.com>
2025-01-16 11:03:53 +0545

    add and configure auth data source

---
+++ lib/app/di/di.dart
@@ -0,0 +1,81 @@
+import 'package:food_delivery_application/core/network/hive_service.dart';
+import 'package:food_delivery_application/features/splash/presentation/view_model/splash_cubit.dart';
+import 'package:get_it/get_it.dart';
+import 'package:softwarica_student_management_bloc/features/auth/data/data_source/local_data_source/auth_local_datasource.dart';
+import 'package:softwarica_student_management_bloc/features/auth/data/repository/auth_local_repository/auth_local_repository.dart';
+import 'package:softwarica_student_management_bloc/features/auth/domain/use_case/login_usecase.dart';
+import 'package:softwarica_student_management_bloc/features/auth/domain/use_case/register_user_usecase.dart';
+import 'package:softwarica_student_management_bloc/features/auth/presentation/view_model/login/login_bloc.dart';
+import 'package:softwarica_student_management_bloc/features/auth/presentation/view_model/signup/register_bloc.dart';
+import 'package:softwarica_student_management_bloc/features/home/presentation/view_model/home_cubit.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initHiveService();
+  await _initHomeDependencies();
+  await _initRegisterDependencies();
+  await _initLoginDependencies();
+  await _initSplashScreenDependencies();
+}
+
+_initHiveService() {
+  getIt.registerLazySingleton<HiveService>(
+    () => HiveService(),
+  );
+}
+
+_initHomeDependencies() async {
+  getIt.registerFactory<HomeCubit>(
+    () => HomeCubit(),
+  );
+}
+
+_initRegisterDependencies() {
+  // init local data source
+  getIt.registerLazySingleton(
+    () => AuthLocalDataSource(getIt<HiveService>()),
+  );
+
+  // init local repository
+  getIt.registerLazySingleton(
+    () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
+  );
+
+  // register use usecase
+  getIt.registerLazySingleton<RegisterUseCase>(
+    () => RegisterUseCase(
+      getIt<AuthLocalRepository>(),
+    ),
+  );
+
+  getIt.registerFactory<RegisterBloc>(
+    () => RegisterBloc(
+      batchBloc: getIt<BatchBloc>(),
+      courseBloc: getIt<CourseBloc>(),
+      registerUseCase: getIt(),
+    ),
+  );
+}
+
+_initLoginDependencies() async {
+  getIt.registerLazySingleton<LoginUseCase>(
+    () => LoginUseCase(
+      getIt<AuthLocalRepository>(),
+    ),
+  );
+
+  getIt.registerFactory<LoginBloc>(
+    () => LoginBloc(
+      registerBloc: getIt<RegisterBloc>(),
+      homeCubit: getIt<HomeCubit>(),
+      loginUseCase: getIt<LoginUseCase>(),
+    ),
+  );
+}
+
+_initSplashScreenDependencies() async {
+  getIt.registerFactory<SplashCubit>(
+    () => SplashCubit(getIt<LoginBloc>()),
+  );
+}
